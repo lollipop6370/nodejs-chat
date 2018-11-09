ee8e9d273c5652dac995c574ff17e2d85a4f3f6d
lollipop6370 <knn101do@gmail.com>
2018-11-09 20:48:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ let onlineCount = 0;
 app.get('/', (req, res) => {
     res.sendFile( __dirname + '/views/index.html');
 });
+app.get('/game', (req, res) => {
+	res.sendFile( __dirname + '/views/game.html');
+});
  
 // 當發生連線事件
 io.on('connection', (socket) => {
@@ -25,11 +28,6 @@ io.on('connection', (socket) => {
         io.emit("msg", msg);
     });
 
-	// 接收來自前端的 greet 事件
-    // 然後回送 greet 事件，並附帶內容
-	socket.on("greet", () => {
-        socket.emit("greet", onlineCount);
-    });
 	
     // 當發生離線事件
     socket.on('disconnect', () => {
@@ -37,10 +35,13 @@ io.on('connection', (socket) => {
         onlineCount = (onlineCount < 0) ? 0 : onlineCount-=1;
         io.emit("online", onlineCount);
     });
+	
+	socket.on("gamestart",()=>{
+		
+	});
 });
 
 
-// 注意，這邊的 server 原本是 app
 server.listen(PORT, () => {
     console.log("Server Started. http://localhost:8080");
 });
